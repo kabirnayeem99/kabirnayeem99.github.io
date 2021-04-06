a2a251c91ada40fbc1993ed3d55b53ed20062e65
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-04-06 23:49:19 +0600

    fix animation glitches

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,34 +28,34 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '100px',
-    duration: 600,
+    distance: '80px',
+    duration: 800,
     reset: true
 });
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {});
-sr.reveal('.button', { delay: 100 });
-sr.reveal('.home__img', { delay: 300 });
-sr.reveal('.home__social-icon', { interval: 100 });
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img', {});
-sr.reveal('.about__subtitle', { delay: 300 });
-sr.reveal('.about__text', { delay: 300 });
+sr.reveal('.about__subtitle', { delay: 400 });
+sr.reveal('.about__text', { delay: 400 });
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {});
 sr.reveal('.skills__text', {});
-sr.reveal('.skills__data', { interval: 100 });
-sr.reveal('.stats__box', { delay: 300 });
+sr.reveal('.skills__data', { interval: 200 });
+sr.reveal('.stats__box', { delay: 400 });
 
 
 /*SCROLL WORK*/
-sr.reveal('.work__img', { interval: 100 });
+sr.reveal('.work__img', { interval: 200 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input', { interval: 100 });
+sr.reveal('.contact__input', { interval: 200 });
 
 
 
