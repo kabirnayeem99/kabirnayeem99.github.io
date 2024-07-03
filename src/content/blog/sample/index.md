a95306105999735a3088bb03a64a8ff6bfdaa572
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-07-03 20:56:56 +0600

    changes

---
+++ src/content/blog/sample/index.md
@@ -0,0 +1,7 @@
+---
+title: "Sample"
+description: "Sample Description"
+date: "Jul 3 2024"
+---
+
+Sample text
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,14 +9,14 @@ export function formatDate(date: Date) {
   return Intl.DateTimeFormat("en-US", {
     month: "short",
     day: "2-digit",
-    year: "numeric"
+    year: "numeric",
   }).format(date);
 }
 
 export function readingTime(html: string) {
   const textOnly = html.replace(/<[^>]+>/g, "");
   const wordCount = textOnly.split(/\s+/).length;
-  const readingTimeMinutes = ((wordCount / 200) + 1).toFixed();
+  const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
   return `${readingTimeMinutes} min read`;
 }
 
